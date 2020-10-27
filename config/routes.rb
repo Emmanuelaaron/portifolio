3bf2a7a4b33f83fd70301f0a9bb5aff36c8c6596
emmanuelaaron <emmanuelisabirye9@gmail.com>
2020-10-27 09:40:22 +0300

    Adds linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'portifolio', to: 'my_portfolio#new'
+  root to: 'my_portfolio#new'
 end
